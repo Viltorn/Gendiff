2ad7c2856c6b1ef51acb0a69616dbe430681ff24
Viltorn <viltorn@gmail.com>
2022-04-06 21:43:53 +0300

    remove mutation from gendiff

--- gendiff.js
+++ gendiff.js
@@ -9,17 +9,23 @@ const getDataDiff = (file1Data, file2Data) => {
   const sortedKeys = _.sortBy(unionKeys);
   const result = sortedKeys.reduce((acc, key) => {
     if (!Object.prototype.hasOwnProperty.call(file1Data, `${key}`)) {
-      acc[key] = { data: file2Data[key], status: 'added' };
-    } else if (!Object.prototype.hasOwnProperty.call(file2Data, `${key}`)) {
-      acc[key] = { data: file1Data[key], status: 'deleted' };
-    } else if (_.isObject(file1Data[key]) && _.isObject(file2Data[key])) {
-      acc[key] = { data: getDataDiff(file1Data[key], file2Data[key]), status: 'nested' };
-    } else if (file1Data[key] !== file2Data[key]) {
-      acc[key] = { oldData: file1Data[key], newData: file2Data[key], status: 'changed' };
-    } else {
-      acc[key] = { data: file1Data[key], status: 'unchanged' };
+      const value = { data: file2Data[key], status: 'added' };
+      return { ...acc, [key]: value };
     }
-    return acc;
+    if (!Object.prototype.hasOwnProperty.call(file2Data, `${key}`)) {
+      const value = { data: file1Data[key], status: 'deleted' };
+      return { ...acc, [key]: value };
+    }
+    if (_.isObject(file1Data[key]) && _.isObject(file2Data[key])) {
+      const value = { data: getDataDiff(file1Data[key], file2Data[key]), status: 'nested' };
+      return { ...acc, [key]: value };
+    }
+    if (file1Data[key] !== file2Data[key]) {
+      const value = { oldData: file1Data[key], newData: file2Data[key], status: 'changed' };
+      return { ...acc, [key]: value };
+    }
+    const value = { data: file1Data[key], status: 'unchanged' };
+    return { ...acc, [key]: value };
   }, {});
   return result;
 };
