068f672db63348845ab19bc063b88320195014c7
Viltorn <viltorn@gmail.com>
2022-04-06 20:38:29 +0300

    delete variables from index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,41 +10,38 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-let result;
-let expectedResult;
-
 test('gendiff json, stylish format', () => {
-  result = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish');
-  expectedResult = readFile('result.txt');
+  const result = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish');
+  const expectedResult = readFile('result.txt');
   expect(result).toEqual(expectedResult);
   console.log(getFixturePath('file1.json'));
   console.log(process.cwd());
 });
 
 test('gendiff yaml, stylish format', () => {
-  result = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'stylish');
-  expectedResult = readFile('result.txt');
+  const result = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'stylish');
+  const expectedResult = readFile('result.txt');
   expect(result).toEqual(expectedResult);
   console.log(process.cwd());
 });
 
 test('gendiff json, plain format', () => {
-  result = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain');
-  expectedResult = readFile('result-plain.txt');
+  const result = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain');
+  const expectedResult = readFile('result-plain.txt');
   expect(result).toEqual(expectedResult);
   console.log(process.cwd());
 });
 
 test('gendiff yaml, plain format', () => {
-  result = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'plain');
-  expectedResult = readFile('result-plain.txt');
+  const result = genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'plain');
+  const expectedResult = readFile('result-plain.txt');
   expect(result).toEqual(expectedResult);
   console.log(process.cwd());
 });
 
 test('gendiff json format', () => {
-  result = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json');
-  expectedResult = readFile('result-json.json');
+  const result = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json');
+  const expectedResult = readFile('result-json.json');
   expect(result).toEqual(expectedResult);
   console.log(process.cwd());
 });
